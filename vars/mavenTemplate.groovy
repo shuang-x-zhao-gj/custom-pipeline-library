8de4bf00f49283e972bd796bc87c827dcdc802c9
szhao <szhao@gaojihealth.com>
2018-07-31 23:22:14 +0800

    disable jenkins-ssh-config

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -41,7 +41,7 @@ def call(Map parameters = [:], body) {
                           secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                           secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                           secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                          secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
+                          //secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
                           secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
                           hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
                 envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
