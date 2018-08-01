7285451ebd0c20cd9bd60c1f0ad1f14b3d55dfe3
szhao <szhao@gaojihealth.com>
2018-08-02 00:33:47 +0800

    try more

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -38,7 +38,7 @@ def call(Map parameters = [:], body) {
                         [name: 'clients', image: "${clientsImage}", command: 'cat', ttyEnabled: true, privileged: true]],
                 volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
                           persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepo'),
-                          secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                          //secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                           secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                           secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                           //secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
