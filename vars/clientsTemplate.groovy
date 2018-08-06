34e47f0b67e3280b1a72167cbb79566f0689aa47
szhao <szhao@gaojihealth.com>
2018-08-06 17:13:05 +0800

    fix clientTemplate

--- vars/clientsTemplate.groovy
+++ vars/clientsTemplate.groovy
@@ -21,14 +21,14 @@ def call(Map parameters = [:], body) {
     } else {
         echo 'Mounting docker socket to build docker images'
         podTemplate(label: label, serviceAccount: 'jenkins', inheritFrom: "${inheritFrom}",
-                containers: [[name: 'clients', image: "${clientsImage}", command: 'cat', privileged: true, ttyEnabled: true, envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]]],
+                containers: [[name: 'clients', image: "${clientsImage}", command: 'cat', privileged: true, ttyEnabled: true, envVars: [envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]]],
                 volumes: [
                         //secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                         secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                         persistentVolumeClaim(claimName: 'git-ssh-key', mountPath: '/root/.ssh', readOnly: true),
                         persistentVolumeClaim(claimName: 'docker-config', mountPath: '/home/jenkins/.docker', readOnly: true),
                         hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-                envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) {
+                envVars: [envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'), envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]) {
             body()
         }
     }
