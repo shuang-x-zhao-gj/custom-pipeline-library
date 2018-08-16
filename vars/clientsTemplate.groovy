bbd5138b25de41f8a54e51841b0b3e793a1542ad
szhao <szhao@gaojihealth.com>
2018-08-16 15:50:40 +0800

    try new base image

--- vars/clientsTemplate.groovy
+++ vars/clientsTemplate.groovy
@@ -4,7 +4,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = "clients.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
     def label = parameters.get('label', defaultLabel)
 
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:0.1')
+    def clientsImage = parameters.get('clientsImage', 'docker-repo.gaojihealth.cn:80/service-base-image/jenkins-slave-nodejs-mobile:v2')
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
     def flow = new io.fabric8.Fabric8Commands()
@@ -28,7 +28,8 @@ def call(Map parameters = [:], body) {
                         persistentVolumeClaim(claimName: 'git-ssh-key', mountPath: '/root/.ssh', readOnly: true),
                         persistentVolumeClaim(claimName: 'docker-config', mountPath: '/home/jenkins/.docker', readOnly: true),
                         hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-                envVars: [envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'), envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')]) {
+                envVars: [envVar(key: 'DOCKER_HOST', value: 'unix:///var/run/docker.sock'), envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')],
+                imagePullSecrets: ['registry-key-secret-1']) {
             body()
         }
     }
